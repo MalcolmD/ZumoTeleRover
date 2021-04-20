d766dede11072b0d6b6fca8242cc231b61def134
malcolm <Yoga>
2021-04-20 10:52:20 -0600

    Automated the ngrok setup

--- install.sh
+++ install.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 CURDIR=$PWD
 
-# Update all our packages and software
-sudo apt-get update && sudo apt-get upgrade
+if [ $# -ne 1 ]; then
 
-# First we need to enable the camera
-sudo raspi-config nonint do_camera 0
+	echo "Please try again with the authentication code provided as an argument to install the Zumo Software"
 
-# Requires cmake
-sudo apt-get install cmake
+else
+	# Update all our packages and software
+	sudo apt-get update && sudo apt-get upgrade -y
 
-# Requires jpeglib
-sudo apt-get install libjpeg9-dev
+	# First we need to enable the camera
+	sudo raspi-config nonint do_camera 0
+
+	# Requires cmake
+	sudo apt-get install cmake
+
+	# Requires jpeglib
+	sudo apt-get install libjpeg9-dev
 
 
-# Install mjpeg-streamer
-cd mjpg-streamer/mjpg-streamer-experimental/
-make
+	# Install mjpeg-streamer
+	cd mjpg-streamer/mjpg-streamer-experimental/
+	make
 
-# Install Python dependencies
-cd $CURDIR
-sudo apt-get install python-pip -y
-python -m pip install bottle
-python -m pip install pyserial
+	# Install Python dependencies
+	cd $CURDIR
+	sudo apt-get install python-pip -y
+	python -m pip install bottle
+	python -m pip install pyserial
 
-# Intialize ngrok
-cd ngrok/
-./ngrok
+	# Initialize ngrok
+	cd ngrok/
+	./ngrok $1
+	
+	echo "" >> ~/.ngrok2/ngrok.yml
+	cat ngrok.yml >> ~/.ngrok2/ngrok.yml
+	
+	# Ask for user/password for control access
+	echo ""
+	echo -n "Please provide a username to access Zumo Control: "
+  read USER
 
-# Add enviroment variables for zumo to run...
-echo "alias ZumoStart=~/ZumoTeleRover/Scripts/ZumoStart.sh" >> ~/.bashrc
-echo "export ZumoDir=/home/pi/ZumoTeleRover/" >> ~/.bashrc
+  echo -n "Please provide a password to access Zumo Control: "
+  read PASS
+
+  printf "\nYour user:pass credentials to access the controls page are...\nuser: ${USER}\npassword: ${PASS}\n"
+	
+	printf "    auth: \"${USER}:${PASS}\"\n" >> ~/.ngrok2/ngrok.yml
+
+	# Add environment variables for zumo to run...
+	echo "alias ZumoStart=~/ZumoTeleRover/Scripts/ZumoStart.sh" >> ~/.bashrc
+	echo "export ZumoDir=/home/pi/ZumoTeleRover/" >> ~/.bashrc
+fi
